1ad6181898c5816632f76d3ffe4ecd147f5d5c4a
thameemk612 <thameemk612@gmail.com>
2020-02-04 16:14:05 +0530

    home new menu fix

--- application/views/static/home.php
+++ application/views/static/home.php
@@ -28,14 +28,15 @@ if(isset($_COOKIE['redir']))
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<title><?=APP_TITLE?></title>
+  <title>
+		<?=APP_TITLE?>
+	</title>
 	<meta
 		name="description"
 		content="<?=APP_META_CONTENT?>"
@@ -49,10 +50,10 @@ if(isset($_COOKIE['redir']))
 	<link rel="stylesheet" type="text/css" href="<?=  base_url("assets/main/")?>css/Home.css" />
 	<link rel="stylesheet" type="text/css" href="<?=  base_url("assets/main/")?>css/common.css" />
 	<link rel="stylesheet" href="<?=  base_url("assets/main/")?>css/normalize.css" />
-	<link rel="stylesheet" type="text/css" href="<?=  base_url("assets/main/")?>css/linkStyles.css" />
+
 	<link rel="stylesheet" href="<?=  base_url("assets/main/")?>css/pater.css" />
 	<link rel="stylesheet" href="<?=  base_url("assets/main/")?>css/revealer.css" />
-	<link rel="stylesheet" href="<?=  base_url("assets/main/")?>css/menu.css">
+	<link rel="stylesheet" href="<?=  base_url("assets/main/")?>css/menuMain.css">
 
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -85,6 +86,32 @@ if(isset($_COOKIE['redir']))
 </head>
 
 <body class="demo-3">
+
+	<svg class="hidden">
+		<symbol id="icon-arrow" viewBox="0 0 24 24">
+			<title>arrow</title>
+			<polygon points="6.3,12.8 20.9,12.8 20.9,11.2 6.3,11.2 10.2,7.2 9,6 3.1,12 9,18 10.2,16.8 " />
+		</symbol>
+		<symbol id="icon-drop" viewBox="0 0 24 24">
+			<title>drop</title>
+			<path
+			  d="M12,21c-3.6,0-6.6-3-6.6-6.6C5.4,11,10.8,4,11.4,3.2C11.6,3.1,11.8,3,12,3s0.4,0.1,0.6,0.3c0.6,0.8,6.1,7.8,6.1,11.2C18.6,18.1,15.6,21,12,21zM12,4.8c-1.8,2.4-5.2,7.4-5.2,9.6c0,2.9,2.3,5.2,5.2,5.2s5.2-2.3,5.2-5.2C17.2,12.2,13.8,7.3,12,4.8z" />
+			<path d="M12,18.2c-0.4,0-0.7-0.3-0.7-0.7s0.3-0.7,0.7-0.7c1.3,0,2.4-1.1,2.4-2.4c0-0.4,0.3-0.7,0.7-0.7c0.4,0,0.7,0.3,0.7,0.7C15.8,16.5,14.1,18.2,12,18.2z" />
+		</symbol>
+		<symbol id="icon-cross" viewBox="0 0 24 23">
+			<title>cross</title>
+			<path
+			  d="M23.865 3.677c.197-.383.164-.818-.008-1.18.048-.41-.06-.827-.448-1.147L22.323.457c-.636-.524-1.632-.689-2.25 0a155.348 155.348 0 0 1-8.797 9.001C9.011 7.342 6.72 5.255 4.443 3.165c-.8-.734-1.956-.503-2.458.37C1.253 3.9.659 4.374.168 5.182c-.233.386-.215.872 0 1.258 1.47 2.635 4.31 4.951 6.646 7.083-.313.28-.623.562-.942.836-3.146 2.702-5.268 4.416-1.331 7.187.053.037.107.029.161.05.076.036.148.06.232.074.026 0 .05.005.075.003.082.007.16.027.243.011 2.117-.415 4.085-2.074 5.872-3.9 1.74 1.715 3.592 3.353 5.63 4.325.485.232 1.063.097 1.436-.227.626.047 1.197-.342 1.484-.901.042-.026.07-.041.116-.07.91-.569.993-1.701.32-2.482-1.522-1.762-3.138-3.438-4.787-5.084 2.968-2.9 6.674-6.027 8.542-9.667z" />
+		</symbol>
+		<symbol id="icon-menu" viewBox="0 0 17.6 9.9">
+			<title>menu</title>
+			<path d="M17.6,1H0V0h17.6V1z M17.6,4.3h-12v1h12V4.3z M17.6,8.9h-6.9v1h6.9V8.9z" />
+		</symbol>
+		<symbol id="icon-cross" viewBox="0 0 10.2 10.2">
+			<title>cross</title>
+			<path d="M5.8,5.1l4.4,4.4l-0.7,0.7L5.1,5.8l-4.4,4.4L0,9.5l4.4-4.4L0,0.7L0.7,0l4.4,4.4L9.5,0l0.7,0.7L5.8,5.1z" />
+		</symbol>
+	</svg>
 	<svg class="hidden">
 		<symbol id="icon-arrow" viewBox="0 0 24 24">
 			<title>arrow</title>
@@ -111,15 +138,15 @@ if(isset($_COOKIE['redir']))
 
 
 			<section>
-				<nav class="links hestia-font desktoponly cl-effect-1" style="background-color:transparent !important;">
-					<a href="<?=base_url()?>events">EVENTS</a>
-					<a href="<?=base_url()?>sponsors">SPONSORS</a>
-					<a href="<?=base_url()?>"><img style="max-height: 75px;position:relative;top:25px" src="<?=  base_url("assets/main/")?>img/logo.png" /></a>
-					<a href="<?=base_url()?>about">ABOUT</a>
+				<nav class="links hestia-font desktoponly" style="background-color:transparent !important;">
+          <a href="<?=base_url()?>events">EVENTS</a>
+          <a href="<?=base_url()?>sponsors">SPONSORS</a>
+          <a href="<?=base_url()?>"><img style="max-height: 75px;position:relative;top:25px" src="<?=  base_url("assets/main/")?>img/logo.png" /></a>
+          <a href="<?=base_url()?>about">ABOUT</a>
           <?php if($this->session->userdata('sess_logged_in')==0){ ?>
           <a href="<?= $google_login_url ?>">LOGIN</a>
           <?php } else{ ?>
-          <a href="<?=base_url()?>myevents">MY EVENTS</a>
+          <a href="<?=base_url()?>myprofile">MY PROFILE</a>
           <?php } ?>
 				</nav>
 
@@ -163,21 +190,20 @@ if(isset($_COOKIE['redir']))
 
 
 		</div>
-		<nav class="menu">
+		<nav class="Mobilemenu">
 			<button class="btn btn--close">
 				<svg class="icon icon--cross">
 					<use xlink:href="#icon-cross"></use>
 				</svg>
 			</button>
-			<ul class="menu__inner">
-				<li class="menu__item"><a class="menu__link" href="<?=base_url()?>events">EVENTS</a></li>
-				<li class="menu__item"><a class="menu__link" href="<?=base_url()?>sponsors">SPONSORS</a></li>
-				<li class="menu__item"><a class="menu__link" href="<?=base_url()?>about">ABOUT</a></li>
-
-				<?php if($this->session->userdata('sess_logged_in')==0){ ?>
-				<li class="menu__item"><a class="menu__link" href="<?= $google_login_url ?>">LOGIN</a></li>
+			<ul class="Mobilemenu__inner">
+        <li class="Mobilemenu__item"><a class="Mobilemenu__link  " href="<?=base_url()?>events">EVENTS</a></li>
+				<li class="Mobilemenu__item"><a class="Mobilemenu__link"  href="#">SPONSORS</a></li>
+				<li class="Mobilemenu__item"><a class="Mobilemenu__link" href="#">ABOUT</a></li>
+					<?php if($this->session->userdata('sess_logged_in')==0){ ?>
+				<li class="Mobilemenu__item"><a class="Mobilemenu__link" href="<?= $google_login_url ?>">LOGIN</a></li>
 				<?php }else { ?>
-				<li class="menu__item"><a class="menu__link" href="<?=base_url()?>myevents">MY EVENTS</a></li>
+					<li class="Mobilemenu__item"><a class="Mobilemenu__link" href="<?=base_url()?>myevents">MY EVENTS</a></li>
 				<?php } ?>
 			</ul>
 		</nav>
@@ -186,7 +212,7 @@ if(isset($_COOKIE['redir']))
 			<script id="custom-vertex" type="x-shader/x-vertex">
 
 
-					vec3 mod289(vec3 x)
+				vec3 mod289(vec3 x)
 					{
 						return x - floor(x * (1.0 / 289.0)) * 289.0;
 					}
@@ -454,7 +480,7 @@ if(isset($_COOKIE['redir']))
 
 	<script>
 		(function() {
-			var navEl = document.querySelector("nav.menu"),
+			var navEl = document.querySelector("nav.Mobilemenu"),
 				revealer = new RevealFx(navEl),
 				closeCtrl = navEl.querySelector(".btn--close");
 
@@ -462,7 +488,7 @@ if(isset($_COOKIE['redir']))
 				.querySelector(".btn--menu")
 				.addEventListener("click", function() {
 					revealer.reveal({
-						bgcolor: "#7f40f1",
+						bgcolor: "#101010",
 						duration: 400,
 						easing: "easeInOutCubic",
 						onCover: function(contentEl, revealerEl) {
@@ -479,7 +505,7 @@ if(isset($_COOKIE['redir']))
 				closeCtrl.removeEventListener("click", closeMenu);
 				navEl.classList.remove("menu--open");
 				revealer.reveal({
-					bgcolor: "#7f40f1",
+					bgcolor: "#101010",
 					duration: 400,
 					easing: "easeInOutCubic",
 					onCover: function(contentEl, revealerEl) {
