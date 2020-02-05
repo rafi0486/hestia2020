423db0b948c4a3377ae407ac6d1558fed9ddcf70
thameemk612 <thameemk612@gmail.com>
2020-02-05 16:40:21 +0530

    19 to 20

--- application/views/dashboard/certificate_view.php
+++ application/views/dashboard/certificate_view.php
@@ -10,7 +10,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <title>Verify Certificate | Hestia'19</title>
+    <title>Verify Certificate | Hestia'20</title>
 </head>
 <body style="background: lightgrey no-repeat center center fixed ; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;height: 100%;min-height:100vh;">
 <div class="container">
