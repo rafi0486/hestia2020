e91e55f7f57ccb19cc7afa6ad07e5b3aafb27e4c
rafi0486 <rafi0486@gmail.com>
2020-02-05 19:07:04 +0530

    email user

--- payment/dbconnect.php
+++ payment/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 //$conn = mysqli_connect("localhost:3306","iedctkmc_hestiaU",'l$_Kj}kK-iScK5)e',"iedctkmc_hestiaDB");
 $conn = mysqli_connect("localhost","root",'Thameem@8123',"hestia20_DB");
-$base_app_url="https://www.hestia.live/;
+$base_app_url="https://www.hestia.live/";
 $base_app_title="Hestia 20";
 $private_api='test_67e4b4a82b8d43d3fa1e4053b2f';
 $private_token='test_893a46e8fa71c68603a657320f9';
