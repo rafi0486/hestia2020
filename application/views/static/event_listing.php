1671c4b9b079ad1ceaaa37da71f9729d841f831f
thameemk612 <thameemk612@gmail.com>
2020-02-07 20:50:13 +0530

    click fix

--- application/views/static/event_listing.php
+++ application/views/static/event_listing.php
@@ -125,12 +125,12 @@
           margin-top: 5vh !important;
         }
       }
-			/* .show-button{
-					 display: block;
+			.show-button{
+					 display: block !important;
 			 }
 			 .box__text-inner{
 					 display: none;
-			 } */
+			 }
     </style>
 	<script>
 		document.documentElement.className = "js";
