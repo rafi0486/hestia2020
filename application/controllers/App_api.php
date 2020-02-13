f2059de0b1b9f7e9651736fcd5ea9842857aff28
rafi0486 <rafi0486@gmail.com>
2020-02-13 19:31:23 +0530

    app like events

--- application/controllers/App_api.php
+++ application/controllers/App_api.php
@@ -52,6 +52,9 @@ class App_Api extends CI_Controller {
     }
 
     function GetEventsByCatLike(){
+        foreach ($_POST as $key => $value) {
+    echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
+}
         $catname=$this->security->xss_clean($this->input->post('id'));
         echo $this->appapi_Model->get_event_details_by_catlike($catname);
     }
