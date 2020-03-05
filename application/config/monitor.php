f1fd8d8259379fc5a823926d5a67f0359b4e0a10
thameemk612 <thameemk612@gmail.com>
2020-03-05 14:52:25 +0530

    pass change

--- application/config/monitor.php
+++ application/config/monitor.php
@@ -13,7 +13,7 @@ $config['monitor_user']='registrations';
 $config['monitor_pass']='$2y$10$PXrR2dp1.NFm5VGlPaKFDe2.nIBk3GwZoYcsEYvgMMvjD87SWGnRe';
 
 $config['accommodation_user']='accommodation';
-$config['accommodation_pass']='$2y$10$D9NkmUKL63YIpszkyDyv3OiVqhWwSh3DRcZSM96vT3vHn2YjBz086';
+$config['accommodation_pass']='$2y$10$eZxCIUjM6qc/b.DLwjg.2uiDGqXdS0PeR7.ZQgGgddUXzi.pkZ1w6';
 
 $config['volunteer_user']='volunteer';
 $config['volunteer_pass']='$2y$10$PFlrwuQHWAjbHzP03W4vj.fxti33GI8yan/ScGwtBlAtWt8fezP6e';
