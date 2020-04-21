51fb549cc46d381b996f99c041c57acd8578545d
thameemk612 <thameemk612@gmail.com>
2020-04-21 13:35:27 +0530

    error handler message

--- payment/errors/error_handle.php
+++ payment/errors/error_handle.php
@@ -48,30 +48,7 @@ $err = curl_error($curl);
 curl_close($curl);
 
 }
-// if ($err) {
-//   echo "cURL Error #:" . $err;
-// } else {
-//   echo $response;
-// }
 
-// curl_setopt_array($curl, array(
-//   CURLOPT_URL => "http://api.msg91.com/api/sendhttp.php?route=4&sender=HESTIA&mobiles=918606020486,918281582725&authkey=168668AYTI6RoQyo5986a03e&message= ERROR ".$date." ".$str."&country=91",
-//   CURLOPT_RETURNTRANSFER => true,
-//   CURLOPT_ENCODING => "",
-//   CURLOPT_MAXREDIRS => 10,
-//   CURLOPT_TIMEOUT => 30,
-//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//   CURLOPT_CUSTOMREQUEST => "GET",
-//   CURLOPT_SSL_VERIFYHOST => 0,
-//   CURLOPT_SSL_VERIFYPEER => 0,
-// ));
-//
-// $response = curl_exec($curl);
-// $err = curl_error($curl);
-//
-// curl_close($curl);
-
-// }
 
 
     function format_error( $errno, $errstr, $errfile, $errline ) {
